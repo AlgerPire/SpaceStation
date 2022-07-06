fecdfde6054597b9f09c36c612f9c7e0d78ceed3
AlgerPire <algerpire0@gmail.com>
2022-07-06 17:43:56 +0200

    "First Version"

--- untitled1/src/main/java/Main.java
+++ untitled1/src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        System.out.println("Welcome To Space Station. The position will be soon in the database.....");
+        System.out.println("Welcom To Space Station. The position will be soon in the database.....");
         HttpFinal httpFinal=new HttpFinal();
         httpFinal.addLocationInDatabase();
 
